ba37d89cdd5d839f6f19f7d12a3e07018523342b
underthebed1102 <121211461+underthebed1102@users.noreply.github.com>
2024-02-21 21:37:51 +0700

    Update bubub.py

--- bubub.py
+++ bubub.py
@@ -3,10 +3,10 @@ from PIL import Image
 import base64
 
 # Set page title and favicon
-st.set_page_config(page_title="Encouragement for Bubub", page_icon=":heart:")
+st.set_page_config(page_title="Semangat Bubub", page_icon=":heart:")
 
 # Load background image
-bg_image = Image.open("your_girlfriend_picture.jpg")
+bg_image = Image.open("https://github.com/underthebed1102/bubub/blob/7c2678fbf0d41906f2017e84eae79dead2f941d5/IMG-20230505-WA0007.jpg")
 st.markdown(
     f"""
     <style>
@@ -19,7 +19,7 @@ st.markdown(
 )
 
 # Add background music
-audio_file = open("your_custom_music.mp3", "rb")
+audio_file = open("https://github.com/underthebed1102/bubub/blob/7c2678fbf0d41906f2017e84eae79dead2f941d5/ost%20mancing%20mania.mp3", "rb")
 audio_bytes = audio_file.read()
 st.audio(audio_bytes, format="audio/mp3", start_time=0)
 
